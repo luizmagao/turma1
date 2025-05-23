c6437bfeceb25df3d5fbb2c6aca6dfe50efaf89c
Luiz Lins <luizlins@MacBook-Air-de-Luiz.local>
2025-05-22 21:52:42 -0300

    WP: adicionando arquivos de 01 a 03 das atividades

---
+++ example03.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, soma;
+
+    cout << "Informe dois números: " << endl;
+    cin >> num1 >> num2;
+    soma = num1 + num2;
+    cout << "Resultado será de " << soma << endl;
+    return 0;
+}
